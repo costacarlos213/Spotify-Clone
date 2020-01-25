107dff442d267df54f091434dff6095d1aa01fca
Tigran Bayburtsyan <tigran@bayburtsyan.com>
2020-01-25 09:03:56 +0400

    minimal system tray application

--- electron/electron.ts
+++ electron/electron.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
+import {InitTray} from './trayWindow';
 
 let mainWindow: Electron.BrowserWindow | null;
 
@@ -31,7 +32,10 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+  InitTray();
+});
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
